c20623af5b87d192b076a9647dffd29bba40ddd7
David Kahler <david.m.kahler@gmail.com>
2023-03-05 07:10:53 +0200

    read DWS monthly data to check - still need to sort without solumn header being retained.

---
+++ monthly.R
@@ -0,0 +1,34 @@
+# pull the DWS monthly data
+# table starts with October
+
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+library(stringr)
+library(tidyr)
+library(latex2exp)
+library(devtools)
+install_github("LimpopoLab/hydrostats", force = TRUE)
+library(hydrostats)
+
+x <- read_csv("G1H020_monthly.csv")
+y <- array(NA, dim = (12*nrow(x)))
+yea <- y
+mon <- y
+NyearTotal <- array(NA, dim = c(nrow(x),2))
+for (i in 1:nrow(x)) {
+     for (j in 1:12){
+          pos <- j+(i-1)*12
+          if (j<=3) {
+               yea[pos] <- x$start[i]
+               mon[pos] <- 9 + j
+          } else {
+               yea[pos] <- x$end[i]
+               mon[pos] <- j - 3
+          }
+          y[pos] <- x[i,2+j][[1]]
+     }
+     NyearTotal[i,1] <- x$end[i]
+     NyearTotal[i,2] <- x$Total[i]
+}
